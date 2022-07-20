936121be61866546dc383c1b4c8dcbe92eb869ee
JongTL <62316827+JongTL@users.noreply.github.com>
2022-07-20 16:11:33 +0900

    #5.1

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,27 @@
-import { authService } from "fBase";
-import React from "react";
+import { authService, dbService } from "fBase";
+import { doc } from "firebase/firestore";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+export default ({userObj}) => {
     const history=useHistory();
     const onLogOutClick=()=>{
         authService.signOut();
         history.push("/");
+    };
+    const getMyNweets=async()=>{   
+        const nweets=await dbService
+            .collection("nweets")
+            .where("creatorId","==",userObj.uid)
+            .orderBy("createdAt")
+            .get();
+        console.log(nweets.docs.map((doc)=>doc.data()));
 
     }
+    useEffect(()=>{
+        getMyNweets();
 
+    },[])
     return (
         <>
             <button onClick={onLogOutClick}>Log Out</button>
